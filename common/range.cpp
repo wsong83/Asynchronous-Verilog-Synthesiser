d428332e05debde93f024b56b632e6296df077ab
Wei Song <wsong83@gmail.com>
2012-02-08 17:06:34 +0000

    netlist::Range ready

---
+++ common/range.cpp
@@ -0,0 +1,96 @@
+/*
+ * Copyright (c) 2011 Wei Song <songw@cs.man.ac.uk> 
+ *    Advanced Processor Technologies Group, School of Computer Science
+ *    University of Manchester, Manchester M13 9PL UK
+ *
+ *    This source code is free software; you can redistribute it
+ *    and/or modify it in source code form under the terms of the GNU
+ *    General Public License as published by the Free Software
+ *    Foundation; either version 2 of the License, or (at your option)
+ *    any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
+ */
+
+/* 
+ * Range declaration and definition
+ * 08/02/2011   Wei Song
+ *
+ *
+ */
+
+#include <cassert>
+#include "range.h"
+#include "averilog/src/averilog.hh"
+
+using namespace netlist;
+
+netlist::Range::Range(int sel)
+  : c(sel), type(TConst) {  }
+
+netlist::Range::Range(const Expression& sel)
+  : v(sel), type(TVar) 
+{ 
+  if(sel.is_valuable()) {	// constant expression, value it now
+    c = sel.get_value();
+    type = TConst;
+  }
+ };
+
+netlist::Range::Range(const Range_Exp& sel)
+  : r(sel), type(TRange) 
+{  
+  if(sel.first == sel.second) {	// only one bit is selected
+    if(sel.first.is_valuable()) { // constant expression, value it now
+      c = sel.first.get_value();
+      type = TConst;
+    } else {			// variable expression
+      v = sel.first;
+      type = TVar;
+    }
+  }   
+};
+
+netlist::Range::Range(const Range_Exp& sel, int ctype)
+  : type(TRange)
+{
+  Range_Exp m_sel;
+  if(ctype == averilog::av_parser::token::oPColon) { // positive colon
+    m_sel.first = sel.first+sel.second;
+    m_sel.second = sel.first;
+  } else {			// negtive colon
+    m_sel.first = sel.first;
+    m_sel.second = sel.first - sel.second;
+  } else {
+    // error
+    assert(1 == 0);
+  }
+  r = m_sel;
+
+  if(m_sel.first == m_sel.second) {	// only one bit is selected
+    if(m_sel.first.is_valuable()) { // constant expression, value it now
+      c = m_sel.first.get_value();
+      type = TConst;
+    } else {			// variable expression
+      v = m_sel.first;
+      type = TVar;
+    }
+  } 
+}
+
+bool netlist::Range::is_valuable() {
+  return type == TConst;
+}
+
+bool netlist::Range::is_single() {
+  return type != TRange;
+}
+  
+
---
+++ common/range.h
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2011 Wei Song <songw@cs.man.ac.uk> 
+ *    Advanced Processor Technologies Group, School of Computer Science
+ *    University of Manchester, Manchester M13 9PL UK
+ *
+ *    This source code is free software; you can redistribute it
+ *    and/or modify it in source code form under the terms of the GNU
+ *    General Public License as published by the Free Software
+ *    Foundation; either version 2 of the License, or (at your option)
+ *    any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
+ */
+
+/* 
+ * Range declaration and definition
+ * 08/02/2011   Wei Song
+ *
+ *
+ */
+
+#ifndef _H_RANGE_
+#define _H_RANGE_
+
+#include <utility>
+#include "expression.h"
+
+namespace netlist {
+
+  typedef pair<Expression, Expression> Range_Exp;
+
+  class Range {
+  public:
+    // constructors
+    Range(int);			/* select by a fix number */
+    Range(const Expression&);	/* select by an expression  */
+    Range(const Range_Exp&);    /* declare or select by a range expression */
+    Range(const Range_Exp&, int); /* select by a range expression using positive or negtive colon */
+
+    // helpers
+    bool is_valuable();
+    bool is_single();
+    
+  private:
+    union {
+      int c;			/* constant */
+      Expression v;		/* variable */
+      Range_Exp r;		/* range expression */
+    };
+
+    enum type_t {
+      TConst, 
+      TVar, 
+      TRange} type;
+    
+
+  };
+
+
+}
+
+#endif
